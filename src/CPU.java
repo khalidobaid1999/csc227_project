509754f06547133beebd154baa02e4aef46237c8
3izzo <obharabi@gmail.com>
2020-04-05 19:33:24 +0300

    added CPU functions

--- src/CPU.java
+++ src/CPU.java
@@ -1,5 +1,61 @@
 public class CPU {
+    private Process runningProcess;
+    private int currentCPUTime;
+    private Memory m;
+    //WAITING FOR MEMORY IMPLEMENTATION 
+    public CPU(Memory m) {
+        this.m = m;
+    }
 
-	
-	
+    public void simulateMachineExecuteCycle() {
+        if (runningProcess.CPUBursts.get(runningProcess.getIOCounter()) == 0) {
+            if (runningProcess.getIOCounter() == runningProcess.IOBursts.size()) {
+                runningProcess.setState(ProcessState.TERMINATED);
+                m.finished.add(runningProcess);
+            } else {
+                runningProcess.setState(ProcessState.WAITING);
+                runningProcess.incrementIOCounter();
+                m.waitingQueue.push(runningProcess);
+            }
+            runningProcess = getFromReadyQueue();
+        } else if (checkForPreemption()) {
+            runningProcess.incrementPreemptionCounter();
+            runningProcess.setState(ProcessState.WAITING);
+            m.waitingQueue.push(runningProcess);
+
+            runningProcess = getFromReadyQueue();
+        }
+        executeRunningProcess(1);
+
+        m.decrementWaitingTime();
+    }
+
+    public void incrementCPUTime(int delta) {
+        this.currentCPUTime += delta;
+        try {
+            Thread.sleep(delta);
+        } catch (Exception e) {
+        }
+    }
+
+
+    private Process getFromReadyQueue() {
+        if (m.readyQueue.empty())
+            return null;
+        Process cp = (m.readyQueue.serve());
+        cp.incrementCPUCounter();
+        cp.setState(ProcessState.RUNNING);
+        cp.setReadyQueueEmtryTime(currentCPUTime);
+        return cp;
+    }
+
+    private boolean checkForPreemption() {
+        return false;
+    }
+
+    private void executeRunningProcess(int time) {
+        int i = runningProcess.getIOCounter();
+        runningProcess.CPUBursts.set(i, runningProcess.CPUBursts.get(i) - time);
+        runningProcess.incrementCPUTime();
+    }
 }
--- src/Process.java
+++ src/Process.java
@@ -1,68 +1,69 @@
 import java.util.ArrayList;
-import java.util.Timer;
 
 public class Process {
 
-	
-	//class attributes
+	// class attributes
 	private ProcessState state;
 	private int id;
-	private ArrayList<Integer> CPUBursts;
-	private ArrayList<Integer> IOBursts;
-	private ArrayList<Integer> memoryUsage;
-	private Timer readyQueueEntryTime;
+	public ArrayList<Integer> CPUBursts;
+	public ArrayList<Integer> IOBursts;
+	public ArrayList<Integer> memoryUsage;
+	private int readyQueueEntryTime;
 	private int CPUCounter;
 	private int IOTotalCounter;
 	private int IOTotalTime;
 	private int memoryWaitCounter;
 	private int terminationKillTime;
 	private int preemptionCounter;
-	
-	
-	
-	//constructor
+	private int totalCPUTime;
+
+	// constructor
 	public Process() {
-		
+
 	}
-	
-	
+
 	public int getCPUBurstTime() {
 		int length = this.CPUBursts.size();
 		int sum_time = 0;
-		for(int i=0; i<length; i++)
+		for (int i = 0; i < length; i++)
 			sum_time += this.CPUBursts.get(i);
 		return sum_time;
 	}
-	
-	
+
+	public void setReadyQueueEmtryTime(int t) {
+		this.readyQueueEntryTime = t;
+	}
+
 	public void setState(ProcessState p) {
 		this.state = p;
 	}
-	
-	
+
 	public void incrementCPUCounter() {
 		this.CPUCounter++;
 	}
-	
-	
+
 	public void incrementIOCounter() {
 		this.IOTotalCounter++;
 	}
-	
-	
+
+	public int getIOCounter() {
+		return IOTotalCounter;
+	}
+
 	public void incrementMemoryWaitCounter() {
 		this.memoryWaitCounter++;
 	}
-	
-	
+
 	public void incrementPreemptionCounter() {
 		this.preemptionCounter++;
 	}
-	
-	
+
 	public void addToTotalIOTime(int time) {
 		this.IOTotalTime += time;
 	}
-	
-	
+
+	public void incrementCPUTime() {
+		this.totalCPUTime++;
+	}
+
 }
