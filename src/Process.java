07a26df8e601648658381d7109bec28bb022e5a9
3izzo <obharabi@gmail.com>
2020-04-06 20:42:35 +0300

    added read file function

--- src/Process.java
+++ src/Process.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Process {
-
+	private static int idCounter = 0;
 	// class attributes
 	private ProcessState state;
+	private String name;
 	private int id;
 	public ArrayList<Integer> CPUBursts;
 	public ArrayList<Integer> IOBursts;
@@ -18,8 +21,24 @@ public class Process {
 	private int totalCPUTime;
 
 	// constructor
-	public Process() {
-
+	public Process(Scanner sc) {
+		this.CPUBursts = new ArrayList<Integer>();
+		this.IOBursts = new ArrayList<Integer>();
+		this.memoryUsage = new ArrayList<Integer>();
+		this.id = idCounter++;
+		sc.nextLine();
+		this.name = sc.nextLine();
+		System.out.println("name: " + name);
+		int m = sc.nextInt();
+		for (int i = 0; i < m; i++) {
+			CPUBursts.add(sc.nextInt());
+			memoryUsage.add(sc.nextInt());
+			IOBursts.add(sc.nextInt());
+		}
+		CPUBursts.add(sc.nextInt());
+		memoryUsage.add(sc.nextInt());
+		if (sc.nextInt() != -1)
+			throw new InputMismatchException();
 	}
 
 	public int getCPUBurstTime() {
@@ -66,4 +85,8 @@ public class Process {
 		this.totalCPUTime++;
 	}
 
+	@Override
+	public String toString() {
+		return name + " " + CPUBursts.toString() + memoryUsage.toString() + IOBursts.toString();
+	}
 }
--- src/Util.java
+++ src/Util.java
@@ -1,5 +1,31 @@
-public class Util {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
-	
-	
+public class Util {
+    public static void main(String[] args) {
+        File f = new File("sampleFile.txt");
+        try {
+            Queue<Process> readFile = readFile(f);
+            while(!readFile.isEmpty())
+                System.out.println(readFile.poll());
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    public static Queue<Process> readFile(File f) throws FileNotFoundException {
+        Queue<Process> q = new LinkedList<Process>();
+        Scanner sc = new Scanner(f);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            Process p = new Process(sc);
+            q.add(p);
+        }
+        sc.close();
+        return q;
+    }
+
 }
