7a77743fd09d49a548c8c0c28605c3213f529e57
Fahd <FahdShalhoub@gmail.com>
2020-04-19 16:26:14 +0300

    no cpi

--- src/Memory.java
+++ src/Memory.java
@@ -4,8 +4,6 @@ import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-import com.sun.org.apache.bcel.internal.generic.CPInstruction;
-
 public class Memory {
 
     public PriorityQueue<Process> readyQueue;
